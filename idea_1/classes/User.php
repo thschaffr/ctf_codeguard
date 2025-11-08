8b766cbd402e7827be79cf2f8546f300cda0a0c0
Thomas Schaffer <147936017+thschaffr@users.noreply.github.com>
2025-11-08 14:30:04 +0100

    Create User.php

---
+++ idea_1/classes/User.php
@@ -0,0 +1,12 @@
+<?php
+class User {
+    public $id;
+    public $username;
+    public $isAdmin = false;
+
+    public function __construct($id, $username) {
+        $this->id       = $id;
+        $this->username = $username;
+    }
+}
+?>
